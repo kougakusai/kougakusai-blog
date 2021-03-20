ac85a15be6bd40eb93b641fbce30fbbf45beeb41
森本拓朗 <ujimattya.mt@gmail.com>
2021-03-21 05:01:03 +0900

    [add]githubpagesにdeployの準備8

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { TypographyStyle } from 'react-typography';
-import typography from '../utils/typography';
+// import { TypographyStyle } from 'react-typography';
+// import typography from '../utils/typography';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <html>
-          <Head>
-            <TypographyStyle typography={typography} />
-          </Head>
+          <Head>{/* <TypographyStyle typography={typography} /> */}</Head>
           <body>
             <Main />
             <NextScript />
