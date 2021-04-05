c7824687775bd17ee8c6efdd356887620608deda
minusvoid <68424763+minusvoid@users.noreply.github.com>
2021-04-05 17:29:13 +0900

    Update_Footer_design.

--- src/comonents/molecules/footermenu.tsx
+++ src/comonents/molecules/footermenu.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 export default function FooterMenu() {
   return (
-    <div className="flex flex-col lg:flex-row divide-solid divide-white divide-x-0 lg:divide-x-2 divide-y-0 space-y-1 lg:space-y-0 lg:mb-8 items-center justify-center text-center justify-items-stretch text-sm h-full w-full">
-      <div className="flex px-8">
+    <div className="flex flex-col lg:flex-row divide-solid divide-white divide-x-0 lg:divide-x-2 divide-y-0 space-y-1 lg:space-y-0 lg:mb-8 justify-items-center justify-start text-left justify-items-start text-sm h-full w-full">
+      <div className="flex flex-initial px-4">
         <a
           className="flex justify-center text-center no-underline hover:no-underline"
           title="Aboutページへ"
@@ -11,7 +11,7 @@ export default function FooterMenu() {
           <p className="text-white m-0">このブログについて</p>
         </a>
       </div>
-      <div className="flex px-8">
+      <div className="flex flex-initial px-4">
         <a
           className="flex justify-center text-center no-underline hover:no-underline"
           title="問い合わせページへ"
@@ -20,7 +20,7 @@ export default function FooterMenu() {
           <p className="text-white m-0">お問い合わせ</p>
         </a>
       </div>
-      <div className="flex px-8">
+      <div className="flex flex-initial px-4">
         <a
           className="flex justify-center text-center no-underline hover:no-underline"
           title="プライバシーポリシーページへ"
