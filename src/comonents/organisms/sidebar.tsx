224ab2a50606b258849fef804a2d67bbdf34040d
森本拓朗 <ujimattya.mt@gmail.com>
2021-04-21 16:00:21 +0900

    [fix]iconで再度エラー、charsetをcharSetへ

--- src/comonents/organisms/sidebar.tsx
+++ src/comonents/organisms/sidebar.tsx
@@ -1,10 +1,10 @@
-// import { relative } from 'path';
-// import SNSButtons from '../molecules/snsbuttons';
+import { relative } from 'path';
+import SNSButtons from '../molecules/snsbuttons';
 
-// export default function SideBar() {
-//   return (
-//     <aside className="hidden md:flex md:h-full justify-center items-center md:ml-16 text-center md:w-16">
-//       <SNSButtons />
-//     </aside>
-//   );
-// }
+export default function SideBar() {
+  return (
+    <aside className="hidden md:flex md:h-full justify-center items-center md:ml-16 text-center md:w-16">
+      <SNSButtons />
+    </aside>
+  );
+}
