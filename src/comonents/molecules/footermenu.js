2863d7331a5c82ce5b5e3c6219716b65897892c6
森本拓朗 <ujimattya.mt@gmail.com>
2021-04-21 17:18:43 +0900

    [fix]footerのリンク修正

--- src/comonents/molecules/footermenu.js
+++ src/comonents/molecules/footermenu.js
@@ -8,31 +8,34 @@ export default function FooterMenu() {
   return (
     <div className="flex flex-col lg:flex-row divide-solid divide-white divide-x-0 lg:divide-x-2 divide-y-0 space-y-1 lg:space-y-0 lg:mb-8 justify-start lg:justify-center text-left lg:text-center text-sm h-full w-full">
       <div className="flex flex-initial px-4">
-        <a
-          className="flex justify-center text-center no-underline hover:no-underline"
-          title="Aboutページへ"
-          href={AboutPageLink}
-        >
-          <p className="text-white m-0">このブログについて</p>
-        </a>
+        <Link href="/about">
+          <a
+            className="flex justify-center text-center no-underline hover:no-underline"
+            title="Aboutページへ"
+          >
+            <p className="text-white m-0">このブログについて</p>
+          </a>
+        </Link>
       </div>
       <div className="flex flex-initial px-4">
-        <a
-          className="flex justify-center text-center no-underline hover:no-underline"
-          title="問い合わせページへ"
-          href={InquiryPageLink}
-        >
-          <p className="text-white m-0">お問い合わせ</p>
-        </a>
+        <Link href="/contact">
+          <a
+            className="flex justify-center text-center no-underline hover:no-underline"
+            title="問い合わせページへ"
+          >
+            <p className="text-white m-0">お問い合わせ</p>
+          </a>
+        </Link>
       </div>
       <div className="flex flex-initial px-4">
-        <a
-          className="flex justify-center text-center no-underline hover:no-underline"
-          title="サイトポリシーページへ"
-          href={PolicyPageLink}
-        >
-          <p className="text-white m-0">サイトポリシー</p>
-        </a>
+        <Link href="/policy">
+          <a
+            className="flex justify-center text-center no-underline hover:no-underline"
+            title="サイトポリシーページへ"
+          >
+            <p className="text-white m-0">サイトポリシー</p>
+          </a>
+        </Link>
       </div>
     </div>
   );
