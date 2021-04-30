6c27e22e4288d8caae6dbeca974d67b90d75a36b
森本拓朗 <ujimattya.mt@gmail.com>
2021-04-30 17:09:47 +0900

    [add]ogp設定追加

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,23 @@ export default class MyDocument extends Document {
         <Head>
           {/* <TypographyStyle typography={typography} /> */}
           <link rel="shortcut icon" href={prefix + '/favicon.ico'} />
+          <meta
+            content="茨城大学の工学部による学園祭、こうがく祭を運営するこうがく祭実行委員会公式ブログです。活動内容等を発信していきます。"
+            name="description"
+          />
+          <meta charSet="utf-8" />
+          <meta property="og:url" content="https://kougakusai.github.io/kougakusai-blog/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="こうがく祭ブログ" />
+          <meta
+            property="og:description"
+            content="茨城大学の工学部による学園祭、こうがく祭を運営するこうがく祭実行委員会公式ブログです。活動内容等を発信していきます。"
+          />
+          <meta property="og:site_name" content="こうがく祭実行委員会公式ブログ" />
+          <meta
+            property="og:image"
+            content=" https://kougakusai.github.io/kougakusai-blog/og.jpg/"
+          />
         </Head>
         <body>
           <Main />
